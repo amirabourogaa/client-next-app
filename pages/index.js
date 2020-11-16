1ce74929d4e5cde71af6da37446cbd8c7a980e22
amirabourogaa <68273575+amirabourogaa@users.noreply.github.com>
2020-11-16 05:55:38 -0800

    slide show added

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,44 @@
 import React from "react";
 import Link from "next/link";
-
-
+import 'bootswatch/dist/slate/bootstrap.min.css';
+import "bootswatch/dist/Cerulean/bootstrap.min.css";
+import { UncontrolledCarousel, Row, Col } from "reactstrap";
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
 import Footer from "components/Footers/Footer.js";
 
+const items = [
+  {
+    src: require("assets/img/naturee.jpg"),
+    altText: "Slide 1",
+    caption: "",
+    header: "",
+    key: "1",
+  },
+  {
+    src: require("assets/img/naturee.jpg"),
+    altText: "Slide 2",
+    caption: "",
+    header: "",
+    key: "2",
+  },
+  {
+    src: require("assets/img/naturee.jpg"),
+    altText: "Slide 3",
+    caption: "",
+    header: "",
+    key: "3",
+  },
+];
 export default function Index() {
   return (
     <>
       <IndexNavbar fixed />
       <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+      <Row>
+    <Col md="8" className="mx-auto">
+      <UncontrolledCarousel items={items} />
+    </Col>
+  </Row>
         <div className="container mx-auto items-center flex flex-wrap">
           <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
             <div className="pt-32 sm:pt-0">
@@ -24,26 +53,29 @@ export default function Index() {
 
 
               </p>
-              
+
+              <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button>
              
             </div>
           </div>
         </div>
-        <img
+         
+        {/* <img
           id='rcorners1'
           className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-900-px"
           src={require("assets/img/naturee2.jpg")}
           alt="..."
-        />
-         <style jsx>{`
+        /> */}
+      <style jsx>{`
 
-        #rcorners1 {
+       #rcorners1 {
 
-          border-radius: 50px;
-         padding : 10px;
-        }
+      border-radius: 50px;
+       padding : 10px;
+      }
         
-      `}</style>
+     `}</style>
+
       </section>
 
       <section className="mt-48 md:mt-40 pb-40 relative bg-gray-200">
@@ -278,6 +310,7 @@ export default function Index() {
             </div>
 
             <div className="w-full md:w-6/12 mr-auto px-4 pt-24 md:pt-0">
+          
               <img
                 alt="..."
                 className="max-w-full rounded-lg shadow-xl"
@@ -294,9 +327,9 @@ export default function Index() {
       </section>
 
       <section className="block relative z-1 bg-gray-700">
-        <div className="container mx-auto">
-          <div className="justify-center flex flex-wrap">
-            <div className="w-full lg:w-12/12 px-4  -mt-24">
+        <div className="container px-4 mx-auto">
+          <div className="flex flex-wrap ">
+            <div className="w-full px-4  -mt-24 w-full px-4 flex-1">
               <div className="flex flex-wrap">
                 <div className="w-full lg:w-4/12 px-4">
                   <h5 className="text-xl font-semibold pb-4 text-center">
@@ -313,7 +346,7 @@ export default function Index() {
                   </Link>
                 </div>
 
-                <div className="w-full lg:w-4/12 px-4">
+                {/* <div className="w-full lg:w-4/12 px-4">
                   <h5 className="text-xl font-semibold pb-4 text-center">
                     Profile Page
                   </h5>
@@ -326,7 +359,7 @@ export default function Index() {
                       />
                     </div>
                   </Link>
-                </div>
+                </div>  */}
 
                 <div className="w-full lg:w-4/12 px-4">
                   <h5 className="text-xl font-semibold pb-4 text-center">
@@ -348,97 +381,25 @@ export default function Index() {
         </div>
       </section>
 
-      <section className="py-20 bg-gray-700 overflow-hidden">
-        <div className="container mx-auto pb-64">
-          <div className="flex flex-wrap justify-center">
-            <div className="w-full md:w-5/12 px-12 md:px-4 ml-auto mr-auto md:mt-64">
-              <div className="text-gray-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                <i className="fas fa-code-branch text-xl"></i>
-              </div>
-              <h3 className="text-3xl mb-2 font-semibold leading-normal text-white">
-                Title
-              </h3>
-              <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-500">
-                Since{" "}
-                <a
-                  href=""
-                  className="text-gray-400"
-                  target="_blank"
-                >
-                  Sub title
-                </a>{" "}
-               Description
-              </p>
-             
-             
-            </div>
-
-            <div className="w-full md:w-4/12 px-4 mr-auto ml-auto mt-32 relative">
-            
-              <i className="fas fa-atom text-gray-800 absolute text-55 -top-150-px -right-100 left-auto opacity-80"></i>
-            </div>
-          </div>
-        </div>
-      </section>
+      {/* <section className="py-20 bg-gray-700 overflow-hidden">
+         <div className="w-full lg:w-4/12 px-4">
+                  <h5 className="text-xl font-semibold pb-4 text-center">
+                    Contact us 
+                  </h5>
+                  <Link href="/landing">
+                    <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                      <img
+                        alt="..."
+                        className="align-middle border-none max-w-full h-auto rounded-lg"
+                        src={require("assets/img/landing.jpg")}
+                      />
+                    </div>
+                  </Link>
+                </div>
+      </section> */}
 
       <section className="pb-16 bg-gray-300 relative pt-32">
-        <div
-          className="-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute h-20"
-          style={{ transform: "translateZ(0)" }}
-        >
-          <svg
-            className="absolute bottom-0 overflow-hidden"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-            version="1.1"
-            viewBox="0 0 2560 100"
-            x="0"
-            y="0"
-          >
-            <polygon
-              className="text-gray-300 fill-current"
-              points="2560 0 2560 100 0 100"
-            ></polygon>
-          </svg>
-        </div>
-
-        <div className="container mx-auto">
-          <div className="flex flex-wrap justify-center bg-white shadow-xl rounded-lg -mt-64 py-16 px-12 relative z-10">
-            <div className="w-full text-center lg:w-8/12">
-              <p className="text-4xl text-center">
-                <span role="img" aria-label="love">
-                  😍
-                </span>
-              </p>
-              <h3 className="font-semibold text-3xl">
-                Do you love this Starter Kit?
-              </h3>
-              <p className="text-gray-600 text-lg leading-relaxed mt-4 mb-4">
-                Cause if you do, it can be yours now. Hit the buttons below to
-                navigate to get the Free version for your next project. Build a
-                new web app or give an old project a new look!
-              </p>
-              <div className="sm:block flex flex-col mt-10">
-                <a
-                  href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/overview/notus?ref=nnjs-index"
-                  target="_blank"
-                  className="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-2 bg-gray-600 active:bg-gray-700 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
-                >
-                  Get started
-                </a>
-                <a
-                  href="https://github.com/creativetimofficial/notus-nextjs?ref=nnjs-index"
-                  target="_blank"
-                  className="github-star sm:ml-1 text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-gray-800 active:bg-gray-700 uppercase text-sm shadow hover:shadow-lg"
-                >
-                  <i className="fab fa-github text-lg mr-1"></i>
-                  <span>Help With a Star</span>
-                </a>
-              </div>
-              <div className="text-center mt-16"></div>
-            </div>
-          </div>
-        </div>
+        
       </section>
       <Footer />
     </>

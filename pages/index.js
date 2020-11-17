59d2debc8981b9699a9871e186bd4fc01a851510
amirabourogaa <68273575+amirabourogaa@users.noreply.github.com>
2020-11-17 09:08:05 -0800

    team members section modified

--- pages/index.js
+++ pages/index.js
@@ -75,90 +75,18 @@ export default function Index() {
       </section>
 
       <section className="mt-48 md:mt-40 pb-40 relative bg-gray-200">
-        <div
-          className="-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute h-20"
-          style={{ transform: "translateZ(0)" }}>
-          <svg
-            className="absolute bottom-0 overflow-hidden"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-            version="1.1"
-            viewBox="0 0 2560 100"
-            x="0"
-            y="0">
-            <polygon
-              className="text-gray-200 fill-current"
-              points="2560 0 2560 100 0 100"></polygon>
-          </svg>
-        </div>
-        <div className="container mx-auto">
-          <div className="flex flex-wrap items-center">
-            <div className="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-32">
-              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-gray-800">
-                <img
-                  alt="..."
-                  src="https://scontent.ftun4-1.fna.fbcdn.net/v/t1.15752-9/125425049_999510163894752_8108865946730156680_n.png?_nc_cat=100&ccb=2&_nc_sid=ae9488&_nc_ohc=KNV6gIGjK8gAX9X6_Iq&_nc_ht=scontent.ftun4-1.fna&oh=dc3494a39c8881bdb967fbf6929944a0&oe=5FDA2EFB"
-                  className="w-250px align-middle rounded-t-lg"
-                />
-                <blockquote className="relative p-8 mb-4">
-                  <svg
-                    preserveAspectRatio="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 583 95"
-                    className="absolute left-0 w-full block h-95-px -top-94-px">
-                    <polygon
-                      points="-30,95 583,95 583,65"
-                      className="text-gray-800 fill-current"></polygon>
-                  </svg>
-                 
-                </blockquote>
-              </div>
-            </div>
-
-          </div>
-        </div>
-
+      <Row>
+         
         <div className="container mx-auto overflow-hidden pb-20">
-          <div className="flex flex-wrap items-center">
-            <div className="w-full md:w-4/12 px-12 md:px-4 ml-auto mr-auto mt-48">
-              <div className="text-gray-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                <i className="fas fa-sitemap text-xl"></i>
-              </div>
+         
               <h3 className="text-3xl mb-2 font-semibold leading-normal">
+              <i class="fas fa-chalkboard-teacher"></i>
                 Team members
               </h3>
-              <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
-                greeting
-              </p>
-              <div className="block pb-6">
-                <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-600 bg-white uppercase last:mr-0 mr-2 mt-2">
-                  Buttons
-                </span>
-                <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-600 bg-white uppercase last:mr-0 mr-2 mt-2">
-                  Inputs
-                </span>
-                <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-600 bg-white uppercase last:mr-0 mr-2 mt-2">
-                  Labels
-                </span>
-                <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-600 bg-white uppercase last:mr-0 mr-2 mt-2">
-                  Menus
-                </span>
-                <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-600 bg-white uppercase last:mr-0 mr-2 mt-2">
-                  Navbars
-                </span>
-                <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-600 bg-white uppercase last:mr-0 mr-2 mt-2">
-                  Pagination
-                </span>
-                <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-600 bg-white uppercase last:mr-0 mr-2 mt-2">
-                  Progressbars
-                </span>
-                <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-600 bg-white uppercase last:mr-0 mr-2 mt-2">
-                  Typography
-                </span>
-              </div>
             </div>
-
-            <div className="w-full md:w-5/12 px-4 mr-auto ml-auto mt-32">
+      </Row>    
+      <Row>
+        <div className="w-full md:w-5/12 px-4 mr-auto ml-auto mt-32">
               <div className="relative flex flex-col min-w-0 w-full mb-6 mt-48 md:mt-0">
                 <img
                   alt="..."
@@ -166,9 +94,65 @@ export default function Index() {
                   className="w-full align-middle rounded-lg absolute shadow-lg max-w-210-px left-260-px -top-160-px"
                 />
               </div>
-            </div>
-          </div>
-        </div>
+         
+        </div>   
+        <div className="w-full md:w-5/12 px-4 mr-auto ml-auto mt-32">
+              <div className="relative flex flex-col min-w-0 w-full mb-6 mt-48 md:mt-0">
+                <img
+                  alt="..."
+                  src={require("assets/img/component-profile-card.png")}
+                  className="w-full align-middle rounded-lg absolute shadow-lg max-w-210-px left-260-px -top-160-px"
+                />
+              </div>
+         
+        </div>   
+        <div className="w-full md:w-5/12 px-4 mr-auto ml-auto mt-32">
+              <div className="relative flex flex-col min-w-0 w-full mb-6 mt-48 md:mt-0">
+                <img
+                  alt="..."
+                  src={require("assets/img/component-profile-card.png")}
+                  className="w-full align-middle rounded-lg absolute shadow-lg max-w-210-px left-260-px -top-160-px"
+                />
+              </div>
+         
+        </div>   
+      </Row> 
+
+       <Row>
+        <div className="w-full md:w-5/12 px-4 mr-auto ml-auto mt-32">
+              <div className="relative flex flex-col min-w-0 w-full mb-6 mt-48 md:mt-0">
+                <img
+                  alt="..."
+                  src={require("assets/img/component-profile-card.png")}
+                  className="w-full align-middle rounded-lg absolute shadow-lg max-w-210-px left-260-px -top-160-px"
+                />
+              </div>
+         
+        </div>   
+        <div className="w-full md:w-5/12 px-4 mr-auto ml-auto mt-32">
+              <div className="relative flex flex-col min-w-0 w-full mb-6 mt-48 md:mt-0">
+                <img
+                  alt="..."
+                  src={require("assets/img/component-profile-card.png")}
+                  className="w-full align-middle rounded-lg absolute shadow-lg max-w-210-px left-260-px -top-160-px"
+                />
+              </div>
+         
+        </div>   
+        <div className="w-full md:w-5/12 px-4 mr-auto ml-auto mt-32">
+              <div className="relative flex flex-col min-w-0 w-full mb-6 mt-48 md:mt-0">
+                <img
+                  alt="..."
+                  src={require("assets/img/component-profile-card.png")}
+                  className="w-full align-middle rounded-lg absolute shadow-lg max-w-210-px left-260-px -top-160-px"
+                />
+              </div>
+         
+        </div>   
+      </Row>      
+            
+           
+        
 
         <div className="container mx-auto px-4 pb-32 pt-48">
           <div className="items-center flex flex-wrap">
@@ -230,11 +214,12 @@ export default function Index() {
                   transform:
                     "scale(1) perspective(1040px) rotateY(-11deg) rotateX(2deg) rotate(2deg)",
                 }}
-                src={require("assets/img/naturee.jpg")}
+                src={require("assets/img/logo.jpg")}
               />
             </div>
           </div>
         </div>
+       
       </section>
 
       <section className="block relative z-1 bg-gray-700">
